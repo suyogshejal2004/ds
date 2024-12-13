306d7a824aa2365522265a4ea907ded45fc90078
suyogshejal2004 <161924832+suyogshejal2004@users.noreply.github.com>
2024-12-13 10:58:14 +0530

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ItemLayout from "./ItemLayout";
+import Image from "next/image";
 import Link from "next/link";
 
 const AboutDetails = () => {
@@ -55,37 +56,45 @@ const AboutDetails = () => {
         </ItemLayout>
 
         <ItemLayout className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}>
-          <img
+          <Image
             className="w-full h-auto"
             src={`https://github-readme-stats.vercel.app/api/top-langs/?username=suyogshejal2004&size_weight=0.5&count_weight=0.5&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="Top Languages"
+            width={500}
+            height={300}
             loading="lazy"
           />
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
-          <img
+          <Image
             className="w-full h-auto"
             src={`https://github-readme-stats.vercel.app/api?username=suyogshejal2004&show_icons=true&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="GitHub Stats"
+            width={500}
+            height={300}
             loading="lazy"
           />
         </ItemLayout>
 
         <ItemLayout className={"col-span-full"}>
-        <img
+          <Image
             className="w-full h-auto"
             src={`https://skillicons.dev/icons?i=kotlin,eclipse,react,tailwind,js,html,css,github,git,vscode,androidstudio,idea,java,mysql,py,replit,sqlite,windows`}
             alt="Skills"
+            width={1000}
+            height={200}
             loading="lazy"
           />
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <img
+          <Image
             className="w-full h-auto"
             src={`https://streak-stats.demolab.com?user=suyogshejal2004&theme=transparent&hide_border=true&ring=FEFE5B&currStreakLabel=FEFE5B`}
             alt="GitHub Streak"
+            width={500}
+            height={300}
             loading="lazy"
           />
         </ItemLayout>
@@ -96,10 +105,12 @@ const AboutDetails = () => {
             target="_blank"
             className="w-full"
           >
-           <img
+            <Image
               className="w-full h-auto"
               src="https://github-readme-stats.vercel.app/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B"
               alt="Pinned Repo"
+              width={500}
+              height={300}
               loading="lazy"
             />
           </Link>
